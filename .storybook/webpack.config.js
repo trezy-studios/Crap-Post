8ca49fa6e836cebeb225f6b116686fafcb9a014f
Trezy <tre@trezy.com>
2019-11-26 15:12:10 -0600

    feat: add SCSS suport to Storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+// Export a function. Accept the base config as the only param.
+module.exports = async ({ config, mode }) => {
+  config.module.rules.push({
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader'],
+    include: path.resolve(__dirname, '../'),
+  })
+
+  return config
+}
