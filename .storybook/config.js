a17bc17309f9a4d7811942dc51d6a2a052805d78
Trezy <tre@trezy.com>
2019-11-26 16:00:41 -0600

    feat: add Storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,3 +1,17 @@
-import { configure } from '@storybook/react'
+import {
+  addDecorator,
+  addParameters,
+  configure,
+} from '@storybook/react'
+import { withA11y } from '@storybook/addon-a11y'
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
+
+addParameters({
+  viewport: {
+    viewports: INITIAL_VIEWPORTS,
+  },
+})
+
+addDecorator(withA11y)
 
 configure(require.context('../stories', true, /.js$/), module)
